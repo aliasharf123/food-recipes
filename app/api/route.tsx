5c6510b53043ea5e73fe6561ab8def1e73b23a5b
aliashraf123 <aliashrafali770@gmail.com>
2023-07-31 13:36:19 +0300

    ramdan 2

--- app/api/route.tsx
+++ app/api/route.tsx
@@ -3,5 +3,12 @@ import data from '../../public/data (1).json'
 export async function GET() {
   
  
-  return NextResponse.json( {data} )
+  return NextResponse.json( data  , {
+    status: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    },
+  })
 }
